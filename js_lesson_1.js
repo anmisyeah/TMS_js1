ec0bcae8c70d363c38505bfe122809775d3089db
Anmisyeah <andrew.mironov.1426@gmail.com>
2024-02-12 17:51:47 +0600

    feat: 2nd task

--- js_lesson_1.js
+++ js_lesson_1.js
@@ -2,4 +2,19 @@
 
 let name = 'Andrei';
 let student = name;
-console.log(name, student)
+console.log(name, student);
+
+
+
+// Задание 2
+let text = 'Hello';
+let counter = 100500;
+let isOpen = true;
+let someValue = null;
+
+let typeText = typeof(text);
+let typeCounter = typeof(counter);
+let typeIsOpen = typeof(isOpen);
+let typeSomeValue = typeof(someValue);
+
+console.log(typeText, typeCounter, typeIsOpen, typeSomeValue);
