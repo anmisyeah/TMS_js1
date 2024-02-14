7914dfcc20a25bc80a9689db564039183e6b827a
Anmisyeah <andrew.mironov.1426@gmail.com>
2024-02-14 15:46:06 +0600

    feat: 2nd next level hwTask

--- js_lesson_1.js
+++ js_lesson_1.js
@@ -133,3 +133,12 @@ let i = 4;
 let j = 3;
 [i, j] = [j, i];
 console.log(i, j)
+
+
+
+let codeWord1 = "обернись";
+let codeWord2 = "неужели";
+let codeWord3 = "огурцы";
+let codeWord4 = "липкие";
+let cipher = codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1];
+console.log(cipher);
