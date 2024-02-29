a67cdc706760a653c276fb9131b42c32b3eb62fd
Anmisyeah <andrew.mironov.1426@gmail.com>
2024-02-29 18:34:29 +0600

    feat: hw lesson3

--- js_lesson3.js
+++ js_lesson3.js
@@ -78,3 +78,73 @@ function strWithoutSpace (str) {
     }
     return newStr
 }
+
+
+
+// Дз 1
+function getSum (param) {
+    let sum = 0;
+    for(let i = 0; i <=param; i++) {
+        sum += i;
+    }
+    return sum
+}
+
+
+
+// Дз 2
+function calculateOverpayment (sum) {
+    const annualInterestRate = 0.17;
+    const numOfYears = 5;
+    let overPayment = sum * annualInterestRate * numOfYears;
+    return overPayment;
+}
+
+
+
+// Дз 3
+function trimString (str, from, to) {
+    return str.slice(from, to + 1)
+}
+
+
+
+// Дз 4
+function getSumNumbers(number) {
+    let sum = 0;
+    while (number > 0) {
+        sum += number % 10;
+        number = Math.floor(number / 10);
+    }
+    return sum;
+}
+
+
+
+// Дз 5
+function getSumma(a,b) {
+    let sum = 0;
+    for (a;a <=b; a++) {
+        sum +=a
+    }
+    return sum
+}
+
+
+
+//Дз 6 
+function fooboo(boolValue, foo, boo) {
+    if(boolValue) {
+        foo()
+    } else {
+        boo()
+    }
+}
+
+function foo() {
+    console.log('foo')
+}
+
+function boo() {
+    console.log('boo')
+}
